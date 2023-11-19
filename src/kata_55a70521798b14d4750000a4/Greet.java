e3220b476c2b5664f3933cb3a0d6a56ad8410d07
Zardexd <Alexander.Raine.Dev@gmail.com>
2023-11-19 17:55:17 +0300

    Kata Solved

---
+++ src/kata_55a70521798b14d4750000a4/Greet.java
@@ -0,0 +1,14 @@
+package kata_55a70521798b14d4750000a4;
+
+//Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+//
+//        [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+public class Greet {
+    public static String greet(String name)
+    {
+
+        return "Hello, "+ name +" how are you doing today?";
+    }
+}
+
